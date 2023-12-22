5a4f98384bc17a5a7ce3662142badfc5ad8649eb
Spencer <spencer.parkin@proton.me>
2023-12-21 23:00:59 -0700

    Almost have options file being read by the options activity.  Time to go to bed.  I've spent too much time on this stupid thing.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.serialization")
 }
 
 android {
@@ -51,6 +52,7 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.games:games-activity:1.2.2")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/spencer/gravitymaze/OptionsActivity.kt
+++ app/src/main/java/com/spencer/gravitymaze/OptionsActivity.kt
@@ -2,10 +2,28 @@ package com.spencer.gravitymaze
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.*
 
 class OptionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_options)
+
+        val jsonString = application.assets.open("options.json").bufferedReader().use {
+            it.readText()
+        }
+        val options = Json.parseToJsonElement(jsonString).jsonObject
+
+        val gravity = options["gravity"]!!.jsonPrimitive.float
+        var bounce = options["bounce"]!!.jsonPrimitive.float
+
+        // TODO: This is wrong.
+        findViewById<SeekBar>(R.id.seekBarGravity).alpha = gravity
+        findViewById<SeekBar>(R.id.seekBarBounce).alpha = bounce
+
+        val i : Int = 0
     }
 }
